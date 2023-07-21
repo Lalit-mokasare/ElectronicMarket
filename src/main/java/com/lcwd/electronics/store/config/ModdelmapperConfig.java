de643f0d8b39a7afd24affbe8ad842adcb37e230
DELL <lmokasare@gmail.com>
2023-07-21 03:14:24 -0700

    ModdelmapperConfig completed by lalit

---
+++ src/main/java/com/lcwd/electronics/store/config/ModdelmapperConfig.java
@@ -0,0 +1,35 @@
+package com.lcwd.electronics.store.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModdelmapperConfig {
+
+
+
+
+
+
+    @Bean
+    public ModelMapper modelMapper() {
+
+        return new ModelMapper();
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
