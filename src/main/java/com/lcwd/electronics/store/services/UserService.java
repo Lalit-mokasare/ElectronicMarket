7ba192e8c87055d28f16b60ac1975aa751b013d3
DELL <lmokasare@gmail.com>
2023-07-21 00:35:49 -0700

    UserService created by lalit

---
+++ src/main/java/com/lcwd/electronics/store/services/UserService.java
@@ -0,0 +1,44 @@
+package com.lcwd.electronics.store.services;
+
+import com.lcwd.electronics.store.dtos.UserDto;
+import com.lcwd.electronics.store.entities.User;
+
+import java.util.List;
+
+
+
+public interface UserService {
+
+    // create
+
+    UserDto createUser(UserDto userDto);
+
+
+
+    // update
+    UserDto updateUser(UserDto userDto, String userId);
+
+    // delete
+    void deleteUser(String userId);
+
+
+    // get all users
+    List<UserDto> getAllUser();
+
+    // get single user by id
+
+    UserDto getUser(String userId);
+
+
+    // get single user by email;
+
+    UserDto getUserByEmail(String email);
+
+    // search user
+
+    List<UserDto> searchUser(String keyword);
+
+    // other user specific feature
+
+
+}
