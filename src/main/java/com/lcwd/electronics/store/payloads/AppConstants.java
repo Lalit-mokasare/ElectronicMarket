dc606daacae21facd0c36d55e3f349a0a78dd872
DELL <lmokasare@gmail.com>
2023-07-21 03:12:07 -0700

    ApiResponseMessage created by lalit

---
+++ src/main/java/com/lcwd/electronics/store/payloads/AppConstants.java
@@ -0,0 +1,23 @@
+package com.lcwd.electronics.store.payloads;
+
+
+
+public class AppConstants {
+
+
+    public static final String DELETE_USER = "User is deleted successfully.........!!;";
+
+    public static final String NOT_FOUND = "User not found given id.....!!";
+
+
+
+
+
+
+
+
+
+
+
+
+}
